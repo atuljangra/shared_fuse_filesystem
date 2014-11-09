e09b79f854c5b1649671d194986bbf26667dc7cb
atuljangra <atuljangra66@gmail.com>
2014-11-09 05:59:24 +0530

    Added the connect request in client

--- client/client.cpp
+++ client/client.cpp
@@ -11,16 +11,10 @@
 #include "../utils.h"
 
 #define SERVER "localhost"
+
 using namespace std;
 
-void error(const char *msg)
-{
-    perror(msg);
-    exit(0);
-}
-
-int main()
-{
+int sendConnectRequest() {
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
@@ -31,15 +25,12 @@ int main()
     portno = CONNECT_PORT;
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     
-    if (sockfd < 0) 
-        error("ERROR opening socket");
+    if (sockfd < 0) {
+        cout << "ERROR opening socket" << endl;
+        return -1;
+    }
     
     server = gethostbyname(SERVER);
-    
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
     
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
@@ -47,21 +38,38 @@ int main()
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
+    
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) { 
+        cout << "ERROR connecting" << endl;
+        return -1;
+    }
+
     bzero(buffer,256);
     stringstream ss;
     ss << CONNECT_CODE << " YOLO" << endl;
     const char * buffer1 = ss.str().c_str();
     cout << "Writing " << buffer1 << endl;
     n = write(sockfd,buffer1,strlen(buffer1));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+    
+    if (n < 0) {
+        cout << "ERROR writing to socket" << endl;
+        return -1;
+    }
+    
     close(sockfd);
+    
+    return 0;
+}
+
+int main()
+{
+    cout << "----Welcome to the shared fuse based filesystem---" << endl;
+    cout << "Lemme first initialize the client" << endl;
+    cout << "Sending Request ...... " << endl;
+    if (sendConnectRequest() != 0) {
+        cout << "Unable to send request! Exiting" << endl;
+        return 0;
+    }
+    cout << "Request Send. Waiting for response now.." << endl;
     return 0;
 }
