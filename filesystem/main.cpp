3e5518e747aff87dbdc99af0033c1f75c27bbdc5
atuljangra <atuljangra66@gmail.com>
2014-11-14 08:27:08 +0530

    Moving.

--- filesystem/main.cpp
+++ filesystem/main.cpp
@@ -1,8 +1,19 @@
 /*
  * This is the main driver program for the filesystem.
  */
+#include <fuse.h>
+#include <cstdio>
+
 #include "wrapper.h"
 
-int main () {
+#define DFUSE_USE_VERSION 26
+
+int main (int argc, char *argv[]) {
+    
+    // Adding fuse bindings;
+
+
+
+    // Running the file system;
     return 0;
 }
