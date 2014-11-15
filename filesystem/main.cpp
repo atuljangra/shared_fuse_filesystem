32e093a23954132718c1f53fc9d8f8359dd517e9
atuljangra <atuljangra66@gmail.com>
2014-11-15 18:56:05 +0530

    Installed fuse handlers

--- filesystem/main.cpp
+++ filesystem/main.cpp
@@ -5,15 +5,46 @@
 #include <cstdio>
 
 #include "wrapper.h"
+struct fuse_operations filesystem_oper;
 
 #define DFUSE_USE_VERSION 26
 
 int main (int argc, char *argv[]) {
     
     // Adding fuse bindings;
-
-
-
+    filesystem_oper.getattr = wrap_getAttr;  
+    filesystem_oper.readlink = wrap_readlink;
+    filesystem_oper.mknod = wrap_mknod;
+    filesystem_oper.mkdir = wrap_mkdir;
+    filesystem_oper.unlink = wrap_unlink;
+    filesystem_oper.rmdir = wrap_rmdir;
+    filesystem_oper.symlink = wrap_symlink;
+    filesystem_oper.rename = wrap_rename;
+    filesystem_oper.link = wrap_link;
+    filesystem_oper.chmod = wrap_chmod;
+    filesystem_oper.chown = wrap_chown;
+    filesystem_oper.utime = wrap_utime;
+    filesystem_oper.truncate = wrap_truncate;
+    filesystem_oper.open = wrap_open;
+    filesystem_oper.read = wrap_read;
+    filesystem_oper.write = wrap_write;
+    filesystem_oper.statfs = wrap_statfs;
+    filesystem_oper.flush = wrap_flush;
+    filesystem_oper.release = wrap_release;
+    filesystem_oper.fsync = wrap_fsync;
+    filesystem_oper.setxattr = wrap_setxattr;
+    filesystem_oper.getxattr = wrap_getxattr;
+    filesystem_oper.listxattr = wrap_listxattr;
+    filesystem_oper.removexattr = wrap_removexattr;
+    filesystem_oper.opendir = wrap_opendir;
+    filesystem_oper.readdir = wrap_readdir;
+    filesystem_oper.releasedir = wrap_releasedir;
+    filesystem_oper.fsyncdir = wrap_fsyncdir;
+    filesystem_oper.init = wrap_init;
+        
+    printf("Attempting network connection.\n"); 
+    // Start the network connection.    
     // Running the file system;
+    //
     return 0;
 }
--- filesystem/wrapper.h
+++ filesystem/wrapper.h
@@ -18,111 +18,113 @@ void setRootDir(const char *path) {
 
 }
 
-int getAttr(const char * path, struct start *) {
+int wrap_getAttr(const char * path, struct stat *) {
     return 0;
 }
 
-int readlink(const char * path, char *link, size_t size) {
+int wrap_readlink(const char * path, char *link, size_t size) {
     return 0;
 
 }  
 
-int mknod(const char * path mode_t mode, dev_t dev) {
+
+int wrap_mknod(const char * path, mode_t mode, dev_t dev) {
     return 0;
 }
 
-int mkdir(const char * path, mode_t mode) {
+int wrap_mkdir(const char * path, mode_t mode) {
     return 0;
 }
 
-int unlink(const char * path) {
+int wrap_unlink(const char * path) {
     return 0;
 }
 
-int rmdir(const char * path) {
+int wrap_rmdir(const char * path) {
     return 0;
 }
 
-int symlink(const char * path, const char *link) {
+int wrap_symlink(const char * path, const char *link) {
     return 0;
 }
 
-int rename(const char * path, const char * newPath) {
+int wrap_rename(const char * path, const char * newPath) {
     return 0;
 }
 
-int link(const char * path, const char *newPath) {
+int wrap_link(const char * path, const char *newPath) {
     return 0;
 }
 
-int chmod(const char * path, mode_t mode) {
+int wrap_chmod(const char * path, mode_t mode) {
     return 0;
 }
 
-int chown(const char * path, uid_t uid, gid_t gid) {
+int wrap_chown(const char * path, uid_t uid, gid_t gid) {
     return 0;
 }
 
-int utime(const char * path, struct utimbuf *ubuf) {
+int wrap_utime(const char * path, struct utimbuf *ubuf) {
     return 0;
 }
 
-int truncate(const char * path, off_t newSize) {
+int wrap_truncate(const char * path, off_t newSize) {
     return 0;
 }
 
-int open(const char * path, struct fuse_file_info *fileInfo) {
+int wrap_open(const char * path, struct fuse_file_info *fileInfo) {
     return 0;
 }
 
-int read(const char * path, char *buf, size_t size, off_t offset, \
-         struct fuse_file_info *fileInfo) {
+int wrap_read(const char * path, char *buf, size_t size, 
+        off_t offset, struct fuse_file_info *fileInfo) {
     return 0;
 }
 
-int write(const char * path, const char *buf, size_t size, off_t offset, 
-          struct fuse_file_info *fileInfo) {
+int wrap_write(const char * path, const char *buf, size_t size,
+        off_t offset, struct fuse_file_info *fileInfo) {
     return 0;
 }
 
-int statfs(const char * path, struct statvfs *statInfo) {
+int wrap_statfs(const char * path, struct statvfs *statInfo) {
     return 0;
 }
 
-int flush(const char * path, struct fuse_file_info *fileInfo) {
+int wrap_flush(const char * path, struct fuse_file_info *fileInfo) {
     return 0;
 }
-int release(const char * path, struct fuse_file_info *fileInfo) {
+int wrap_release(const char * path, struct fuse_file_info *fileInfo) {
     return 0;
 }
-int fsync(const char * path, int datasync, struct fuse_file_info *fileInfo) {
+int wrap_fsync(const char * path, int datasync, struct fuse_file_info *fileInfo) {
     return 0;
 }
-int setxattr(const char * path, const char *name, const char *value, size_t size) {
+int wrap_setxattr(const char * path, const char *name, const char *value, size_t size, int flags) {
     return 0;
 }
-int getxattr(const char * path, const char *name, char *value, sizE_t size) {
+int wrap_getxattr(const char * path, const char *name, char *value, size_t size) {
+    
     return 0;
 }
-int listxattr(const char * path, char *list, size_t size) {
+int wrap_listxattr(const char * path, char *list, size_t size) {
     return 0;
 }
-int removexattr(const char * path, const char *name) {
+int wrap_removexattr(const char * path, const char *name) {
     return 0;
 }
-int opendir(const char * path, struct fuse_file_info *fileInfo) {
+int wrap_opendir(const char * path, struct fuse_file_info *fileInfo) {
     return 0;
 }
-int readdir(const char * path, void *buf, fuse_fill_dir_t filler, off_t offset,
+int wrap_readdir(const char * path, void *buf, fuse_fill_dir_t filler, off_t offset,
             struct fuse_file_info *fileInfo) {
     return 0;
 }
-int releasedir(const char * path, struct fuse_file_info *fileInfo) {
+int wrap_releasedir(const char * path, struct fuse_file_info *fileInfo) {
     return 0;
 }
-int fsyncdir(const char * path, int datasync, struct fuse_file_info *fileInfo) {
+int wrap_fsyncdir(const char * path, int datasync, struct fuse_file_info *fileInfo) {
     return 0;
 }
-int inti(struct fuse_conn_info *conn) {
+void *wrap_init(struct fuse_conn_info *conn) {
     return 0;
 }
